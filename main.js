a2d28a05bdea799fa1b90173a65e240d311f8411
parmarsanjay9 <100059162+parmarsanjay9@users.noreply.github.com>
2022-11-04 20:49:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,60 @@
+prediction_1 = ""
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format: 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach( '#camera' );
+
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+
+//create your model and store it in var classifier 
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/0F4y1Hq1Q/model.json', modelLoaded);
+
+//define function modelLoaded
+function modelLoaded(){
+    console.log('model loaded!')
+}
+
+//define function check() 
+function check(){
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+}
+
+//define function gotResult(error, results)
+function gotResult(error, results){
+    if(error){
+        console.error(error);
+    } else {
+        console.log(results);
+        document.getElementById("status").innerHTML = results[0].label;
+        prediction_1 = results[0].label;
+
+       if(results[0].label == "proper mask"){
+        document.getElementById("update_emoji").innerHTML = "&#x1F637;";
+       }
+
+       if(results[0].label == "improper mask"){
+        document.getElementById("update_emoji").innerHTML = "&#x26d4;";
+       }
+
+       if(results[0].label == "no mask"){
+        document.getElementById("update_emoji").innerHTML = "&#x26d4;";
+       }
+
+       
+    }
+}
